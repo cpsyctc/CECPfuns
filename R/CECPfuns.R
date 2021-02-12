34db4a72944b02d025d57dc56d0a8b01c35314aa
Chris Evans <chris@psyctc.org>
2021-02-12 10:38:44 +0000

    Temporary increment exploring use of pkgdown and trying to create package documentation.

---
+++ R/CECPfuns.R
@@ -0,0 +1,9 @@
+#' CECPfuns: A package of utility functions
+#'
+#' The CECPfuns package provides functions I, Chris Evans and Clara Paz use a lot
+#'
+#' @section CECPfuns functions:
+#' The functions ...
+#'
+#' @docType package
+#' @name CECPfuns
